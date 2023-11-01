6af323fda1b776e57019a90bb7e1edf588121a5b
Tudor Lechințan <lechintantudor02@gmail.com>
2023-11-01 13:13:22 +0200

    Fix resolve_path

--- crates/cool_resolve/src/item/item_error.rs
+++ crates/cool_resolve/src/item/item_error.rs
@@ -16,6 +16,9 @@ pub enum ItemError {
     #[display("Item is not a module")]
     NotModule { item_id: ItemId },
 
-    #[display("Item is not a module")]
+    #[display("Item is not accessibe from the current module")]
     NotAccessible { item_id: ItemId },
+
+    #[display("Path contains too many super keywords")]
+    TooManySuper,
 }
--- crates/cool_resolve/src/item/resolve.rs
+++ crates/cool_resolve/src/item/resolve.rs
@@ -1,27 +1,38 @@
 use crate::{ItemError, ItemId, ItemResult, ModuleId, ResolveContext};
 use cool_lexer::{sym, Symbol};
-use std::slice;
 
 impl ResolveContext<'_> {
     pub fn resolve_path(&self, module_id: ModuleId, path: &[Symbol]) -> ItemResult<ItemId> {
         let module = &self.modules[module_id];
-        let (&first_symbol, other_symbols) = path.split_first().unwrap();
 
-        let mut path: &[Symbol] = if first_symbol == sym::kw_crate {
-            slice::from_ref(&self.get_module_path(module_id)[0])
-        } else if self[module_id].elems.contains_key(&other_symbols[0]) {
+        let mut current_path: &[Symbol] = if path[0] == sym::kw_crate {
+            &self.get_module_path(module_id)[..1]
+        } else if path[0] == sym::kw_super {
+            let path = self.get_module_path(module_id);
+            &path[..(path.len() - 1)]
+        } else if path.len() >= 2 && self[module_id].elems.contains_key(&path[1]) {
             self.get_module_path(module_id)
         } else {
-            slice::from_ref(&first_symbol)
+            &path[..1]
         };
 
-        for symbol in other_symbols {
-            let module_id = self.get_module_by_path(path)?;
+        for &symbol in &path[1..] {
+            if symbol == sym::kw_super {
+                if current_path.is_empty() {
+                    return Err(ItemError::TooManySuper);
+                }
 
-            let item = self.modules[module_id]
-                .elems
-                .get(symbol)
-                .ok_or_else(|| ItemError::NotFound { path: path.into() })?;
+                current_path = &current_path[..(current_path.len() - 1)];
+                continue;
+            }
+
+            let module_id = self.get_module_by_path(current_path)?;
+
+            let item = self.modules[module_id].elems.get(&symbol).ok_or_else(|| {
+                ItemError::NotFound {
+                    path: current_path.into(),
+                }
+            })?;
 
             if !item.is_exported && !self.is_child_item(module.item_id, item.item_id) {
                 return Err(ItemError::NotAccessible {
@@ -29,10 +40,10 @@ impl ResolveContext<'_> {
                 });
             }
 
-            path = self.get_path(item.item_id);
+            current_path = self.get_path(item.item_id);
         }
 
-        self.get_item_by_path(path)
+        self.get_item_by_path(current_path)
     }
 
     fn get_item_by_path(&self, path: &[Symbol]) -> ItemResult<ItemId> {
