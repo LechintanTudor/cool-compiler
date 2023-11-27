69f3d58ffd3b41ca29cd7cbda6e8fc3d2e8cdece
Tudor Lechințan <lechintantudor02@gmail.com>
2023-11-27 17:50:53 +0200

    Implement parsing for tuple and item types

--- crates/cooler_parser/src/ty/mod.rs
+++ crates/cooler_parser/src/ty/mod.rs
@@ -1,16 +1,37 @@
-use crate::{ParseResult, Parser};
+mod item_ty;
+mod tuple_ty;
+
+pub use self::item_ty::*;
+pub use self::tuple_ty::*;
+
+use crate::{IdentPath, ParseResult, Parser};
 use cool_collections::define_index_newtype;
 use cool_derive::Section;
+use cool_lexer::{tk, TokenKind};
+use derive_more::From;
 
 define_index_newtype!(TyId);
 
-#[derive(Clone, Section, Debug)]
+#[derive(Clone, Section, From, Debug)]
 pub enum Ty {
-    // Empty
+    Item(ItemTy),
+    Paren(ParenTy),
+    Path(IdentPath),
+    Tuple(TupleTy),
 }
 
 impl Parser<'_> {
     pub fn parse_ty(&mut self) -> ParseResult<TyId> {
-        todo!()
+        let peeked_token = self.peek();
+
+        match peeked_token.kind {
+            TokenKind::Ident(_) => {
+                let path_ty = Ty::Path(self.parse_ident_path()?);
+                Ok(self.data.tys.push(path_ty))
+            }
+            tk::kw_alias | tk::kw_module => self.parse_item_ty(),
+            tk::open_paren => self.parse_paren_or_tuple_ty(),
+            _ => todo!(),
+        }
     }
 }
